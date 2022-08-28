5d6cd2382618cc4862326eca31cf3bad8f3d6a9d
jjorissen52 <jjorissen52@gmail.com>
2022-08-28 13:55:59 -0500

    refactor: get_arg_names becomes its own helper method

--- qgenda/helpers.py
+++ qgenda/helpers.py
@@ -1,7 +1,8 @@
 import inspect
-import sys
+import functools
 
 
+@functools.cache
 def get_default_args(func):
     signature = inspect.signature(func)
     return {
@@ -11,16 +12,21 @@ def get_default_args(func):
     }
 
 
+@functools.cache
+def get_arg_names(method):
+    """
+    Returns any explicitly named positional or keyword arguments defined in
+    the function signature.
+    """
+    return method.__code__.co_varnames[:method.__code__.co_argcount]
+
+
 def named_method_params(method, args, kwargs):
     """
-    Takes a method and its arguments and return a dictionary of all values that will be passed to it. This allows for
-    easy inspection.
-    :param method:
-    :param args:
-    :param kwargs:
-    :return:
+    Takes a method and its arguments and return a dictionary of all values that will be
+    passed to it. This allows for easy inspection.
     """
-    args_names = method.__code__.co_varnames[:method.__code__.co_argcount]
+    args_names = get_arg_names(method)
     params = get_default_args(method)
     args_dict = {**dict(zip(args_names, args)), **kwargs}
     params.update(args_dict)
