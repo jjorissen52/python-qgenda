1fe2dd300268627ae91b7d1b0e61ae4562592378
Ryley B <ryleyb@gmail.com>
2022-09-21 10:41:55 -0700

    Remove caching on helpers
    Some test maintenance as well

--- qgenda/tests/test_exceptions.py
+++ qgenda/tests/test_exceptions.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import json
 os.environ['QGENDA_CONF_REGION'] = 'qgenda_test'  # ensure bad imports
 from qgenda.api.client import QGendaClient
 from qgenda.api import exceptions
@@ -16,7 +17,7 @@ class TestRaises(unittest.TestCase):
             QGendaClient(None, 'password', company_key=None, raise_errors=True)
 
     def test_bad_login(self):
-        with self.assertRaises(exceptions.APICallError):
+        with self.assertRaises(exceptions.HTTPError):
             client = QGendaClient('username', 'password', 'company_key', api_version='v2',
                                   api_url='https://api.qgenda.com/', raise_errors=True)
             client.authenticate()
@@ -27,8 +28,9 @@ class TestNoRaise(unittest.TestCase):
     def test_bad_login(self):
         client = QGendaClient('username', 'password', 'company_key', api_version='v2',
                               api_url='https://api.qgenda.com/', raise_errors=False)
-        client.authenticate()
-        self.assertTrue(all(key in client.auth_details for key in ["error", "error_description"]))
+        resp = client.authenticate()
+        json_resp = json.loads(resp.text)
+        self.assertTrue(all(key in json_resp for key in ["error", "error_description"]))
 
 
 if __name__ == '__main__':
